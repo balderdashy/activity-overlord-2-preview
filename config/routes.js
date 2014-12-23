07d1b3e39906b7c3ec7b5ea6686e11fa2d422b6c
Mike McNeil <mike@balderdash.co>
2014-12-23 11:05:49 -0600

    Disable blueprints.

--- config/routes.js
+++ config/routes.js
@@ -22,21 +22,6 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  // '/': {
-  //   view: 'homepage'
-  // }
-  // 
-
   'get /': 'StaticController.serveSPA',
 
   'get /user/new': 'UserController.new',
@@ -44,16 +29,5 @@ module.exports.routes = {
   'post /user/create': 'UserController.create',
 
   'get /auth/destroy': 'AuthController.destroy'
-
-
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  *  If a request to a URL doesn't match any of the custom routes above, it  *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
 
 };
