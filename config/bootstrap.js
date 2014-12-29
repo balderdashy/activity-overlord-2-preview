b4e6d2bb5ba8744fcc3b011d00780050ff35e7e0
Irl Nathan <irlnathan@gmail.com>
2014-12-29 12:40:14 -0600

    set numSocketsConnected to 0 in bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,17 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  // 
+  User.update({}, {
+      numSocketsConnected: 0
+    },
+    function userUpdated(err, users) {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(users);
+      }
+      cb();
+    }
+  )
 };
