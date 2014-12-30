d55b0e8e179056227488e3ee243bc7e364bf1311
Mike McNeil <mike@balderdash.co>
2014-12-30 13:01:28 -0600

    A few tweaks.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,59 +11,51 @@
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-
-
   User.update({}, {
-      numSocketsConnected: 0
-    },
-    function userUpdated(err, users) {
-      if (err) {
-        console.log(err);
-      } else {
-        // console.log(users);
+    numSocketsConnected: 0
+  }, function userUpdated(err, users) {
+    if (err) {
+      return cb(err);
+    }
+
+    User.findOne({
+      admin: true
+    }).exec(function(err, admin) {
+      if (err) return cb(err);
+      if (admin) {
+        return cb();
       }
 
-      User.findOne({
-        admin: true
-      }).exec(function(err, admin) {
-        if (err) return cb(err);
-        if (admin) {
-          return cb(null, admin);
+      // ****************************************
+
+      require('machinepack-passwords').encryptPassword({
+        password: '123456'
+      }).exec({
+        error: function (err) {
+          return cb(err);
+        },
+        success: function (encryptedPassword) {
+
+          // Create a User with the params sent from
+          // the sign-up form --> new.ejs
+          User.create({
+            name: 'John Galt',
+            title: 'Admin',
+            email: 'a@a.com',
+            admin: true,
+            encryptedPassword: encryptedPassword,
+            lastLoggedIn: new Date()
+          }, function userCreated(err, newUser) {
+            if (err) {
+              return cb(err);
+            }
+            cb();
+          });
         }
-
-        // ****************************************
-
-        require('machinepack-passwords').encryptPassword({
-          password: '123456'
-        }).exec({
-          error: function (err) {
-            console.log(err);
-          },
-          success: function (encryptedPassword) {
-
-            // Create a User with the params sent from
-            // the sign-up form --> new.ejs
-            User.create({
-              name: 'John Galt',
-              title: 'Admin',
-              email: 'a@a.com',
-              admin: true,
-              encryptedPassword: encryptedPassword,
-              lastLoggedIn: new Date()
-            }, function userCreated(err, newUser) {
-              if (err) {
-                cb(err);
-              }
-              cb();
-            });
-          }
-        });
-
-        // ****************************************
-        
       });
-    }
-  )
+
+      // ****************************************
+
+    });
+  });
 };
